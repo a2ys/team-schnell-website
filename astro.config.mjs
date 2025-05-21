02e850ea4b182cf70313db74197f53bae1050726
Aayush Shukla <oreus.aks@gmail.com>
2025-05-21 16:35:10 +0530

    chore: add vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,20 @@ import react from "@astrojs/react";
 
 import sitemap from "@astrojs/sitemap";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://schnell.dunder.in",
+
   vite: {
     plugins: [tailwindcss()],
   },
 
   integrations: [react(), sitemap()],
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
 });
